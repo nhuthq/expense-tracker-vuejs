8e98d51331c1ca6b6c2dec786b8684b3e0d690a2
nhuthq <nhuthq@zigexn.vn>
2024-10-01 11:35:11 +0700

    init UI

---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,17 @@
+<template>
+  <h3>Add new transaction</h3>
+  <form>
+    <div class="form-control">
+      <label for="title">Text</label>
+      <input id="title" type="text" placeholder="Enter text..." />
+    </div>
+    <div class="form-control">
+      <label for="amount"
+        >Amount<br />
+        (negative - expense, positive - income)</label
+      >
+      <input id="amount" type="number" placeholder="Enter amount..." />
+    </div>
+    <button class="btn">Add transaction</button>
+  </form>
+</template>
